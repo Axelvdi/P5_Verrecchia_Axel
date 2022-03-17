8a5e1b1617ff5f9dbd25f62c4e4cb6e18c594498
Axelvdi <axel.v946@gmail.com>
2022-03-17 11:19:32 +0100

    creation script.js and add product

---
+++ front/js/script.js
@@ -0,0 +1,33 @@
+fetch ("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => addProducts(data))
+
+
+function addProducts(data) {
+    data.forEach(element => {
+        let items = document.getElementById('items');
+            a = document.createElement('a');
+            article = document.createElement('article');
+                article.classList.add("productCard")
+            img = document.createElement('img');
+                img.classList.add("productImage")
+            h3 = document.createElement('h3');
+                h3.classList.add("productName");
+            p = document.createElement('p');
+                p.classList.add("productDescription");
+            elementUrl = "./product.html?id=" + element._id
+
+            items.appendChild(a);
+            a.appendChild(article);
+            article.appendChild(img);
+            article.appendChild(h3);
+            article.appendChild(p);
+            a.setAttribute ("href", elementUrl);
+
+            a = element._id;
+            img.src = element.imageUrl;
+            img.alt = element.altTxt;
+            h3.innerHTML = element.name;
+            p.innerHTML = element.description;
+    });
+}
